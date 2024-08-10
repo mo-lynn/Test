70a5d330fceee3db74e6545621112e815e085dda
molynn <60120567+mo-lynn@users.noreply.github.com>
2024-08-10 13:44:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def greet(name):
-    return f"Hello, {name}!"
+def is_even(number):
+    return number % 2 == 0
 
 if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    print(greet(user_name))
+    print("is_even(4):", is_even(4))  # 预期输出：True
