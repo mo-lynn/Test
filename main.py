748fe73207617c51caff237174c66ba973a2cd73
kemo-cs <2594849257@qq.com>
2024-08-11 02:08:48 +0800

    将判断奇数的函数修改正确

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def is_even(number):
 
 # 判断一个数是否是奇数
 def is_odd(number):
-    return number % 2 == 2  # 错误：应为1
+    return number % 2 == 1  # 错误：应为1
 
 if __name__ == "__main__":
     print("is_even(4):", is_even(4))  # 预期输出：True
